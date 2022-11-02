4c0b6b06d61047e8e2d18c01126ae4bb4974cee4
AlenaFiddler <zigafiddler@mail.ru>
2022-11-02 20:59:31 +0100

    Check github pages

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,3 @@
-const path = require('path')
-const fs = require('fs')
-
 module.exports = {
-    publicPath: process.env.NODE_ENV === "production" ? "/alenafiddler.github.io/" : "/",
+    publicPath: process.env.NODE_ENV === "production" ? "/alenafiddler.github.io/dist" : "/",
 };
